aea1dc79fc0ab35a91327d83fc7f5e66e489cff7
nazneen <nazneen@nazneen-ThinkPad-L430.(none)>
2013-10-06 17:59:23 -0500

    Implemented 3PC naive

---
+++ dc_project/src/ut/distcomp/framework/Controller.java
@@ -0,0 +1,84 @@
+package ut.distcomp.framework;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Controller {
+	static ArrayList<Process> listParticipant;
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException{
+		final Config host_conf = new Config("/home/nazneen/workspace/threepc/config.properties");
+		final NetController host_nc = new NetController(host_conf);
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+		    public void run() {
+		    	for(int i=1;i<host_conf.numProcesses;i++)
+					host_nc.sendMsg(i, "SHUTDOWN");
+		    	for(Process p: listParticipant)
+		    		p.destroy();
+		    	try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    	host_nc.shutdown();
+		    	host_conf.logger.info("shutting down");
+		    	}
+		 });
+		host_conf.procNum = 0;
+		int participants = host_conf.numProcesses;
+		List<List<String>> recvdMsg;
+		listParticipant= new ArrayList<Process>();	
+		for(int i = 1; i <participants; i++){
+			Process p = Runtime.getRuntime().exec("java -cp /home/nazneen/workspace/threepc/dc_project/bin/ ut.distcomp.framework.Participant "+i);
+			/*BufferedReader err=new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			String s;
+			while ((s=err.readLine())!=null)
+		    {
+		           System.out.println(s);
+		    }*/
+			listParticipant.add(i-1,p);
+		}
+		
+		Thread.sleep(1000);
+		int c= findCoordinator();
+		//Iterator it = recvdMsg.iterator();
+		String command = "add";
+		String s1 = "a";
+		String s2 = "a.song";
+		host_nc.sendMsg(c, "INVOKE_3PC##"+command+"##"+s1+"##"+s2);
+		while(true){
+			recvdMsg = host_nc.getReceivedMsgs();
+			
+			if(!recvdMsg.isEmpty())
+				break;
+		}
+		//System.out.println("Outside break");
+		//System.out.println(recvdMsg.get(0));
+		if(recvdMsg.get(0).get(1).equals("COMMIT")){
+			host_conf.logger.info("Committed");
+		}
+		else if(recvdMsg.get(0).get(1).equals("ABORT")){
+			host_conf.logger.info("Command Aborted");
+		}
+		
+		//listParticipant.get(c).invokeThreePC(1, "a", "http://a.song");
+		/*
+		for(Participant p:listParticipant){
+			p.shutdown();
+		}
+		*/	
+		host_conf.logger.info("Shutting down");
+		System.exit(0);
+	}
+	
+	private static int findCoordinator(){
+		//TODO find first active process
+		return 1;
+	}
+}
--- dc_project/src/ut/distcomp/framework/Participant.java
+++ dc_project/src/ut/distcomp/framework/Participant.java
@@ -1,19 +1,219 @@
 package ut.distcomp.framework;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import sun.awt.geom.Curve;
+
 public class Participant {
-	int id;
+	static int id;
 	int viewNumber;
 	PlayList playList;
-	final Config conf;
-	
-	public Participant(int id,Config conf){
+	static Config conf;
+	static NetController nc;  
+	static FileHandler fh;
+	static List<List<String>> participant_recvdMsg;
+
+	public static void main(String[] args) throws FileNotFoundException, IOException{ 
+		id=Integer.parseInt(args[0]);
+		conf = new Config("/home/nazneen/workspace/threepc/config.properties");
+		conf.logger.info("Process "+id+" started");
+		try {  
+			// This block configure the logger with handler and formatter  
+			fh = new FileHandler("/home/nazneen/logs/participant_"+id+".log");  
+			conf.logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();  
+			fh.setFormatter(formatter); 
+			conf.logger.info("Process "+id + " initialized");
+		} catch (SecurityException e) {  
+			e.printStackTrace();  
+		} catch (IOException e) {  
+			e.printStackTrace();  
+		}  
+		conf.procNum=id;
+		nc = new NetController(conf);
+		Boolean flag=true;
+		while(flag){
+			List<String> msg_history = null;
+			participant_recvdMsg = nc.getReceivedMsgs();
+			if(!participant_recvdMsg.isEmpty()){
+				String[] message = null;
+				for(List<String> command: participant_recvdMsg){
+					message = command.get(1).split("##");
+					//msg_history.add(message[0]);
+					//TODO Check message order
+					switch(message[0]){
+					case "INVOKE_3PC":
+						conf.logger.info("Received "+command+" from Controller");
+						Boolean finalDecision = invokeThreePC(message[1],message[2],message[3]);
+						if(finalDecision)
+							nc.sendMsg(0,"COMMIT");
+						else
+							nc.sendMsg(0,"ABORT");
+						break;
+					case "VOTE_REQ":
+						log("Received VOTE_REQ");
+						castVote(message[1],message[2],message[3]);
+						waitForDecision();
+						break;
+					case "SHUTDOWN":
+						nc.shutdown();
+						flag=false;
+						break;
+					default:
+						conf.logger.severe("invalid msg received " + message[0]);
+					}
+				}
+			}
+		}
+	}
+
+	private static void waitForDecision() {
+		while(true){
+			List<List<String>> recMsg = nc.getReceivedMsgs();
+			for(List<String> s : recMsg){
+				if(!recMsg.isEmpty()){
+					String msg = s.get(1);
+					if(msg.equals("ABORT")){
+						log("ABORTING");
+						return;
+					}
+					else if(msg.equals("PRECOMMIT")){
+						log("Received PRECOMMIT");
+						nc.sendMsg(1, "ACK");
+						while(true){
+							List<List<String>> commitMsg = nc.getReceivedMsgs();
+							if(!commitMsg.isEmpty()){
+								log("Committing");
+								return;
+							}
+						}
+					}
+
+				}
+
+			}
+		}
+	}
+
+	private static void castVote(String command, String param1, String param2) {
+		switch(command){
+		case "add":
+			conf.logger.info("Received add command");
+			break;
+		case "delete":
+			conf.logger.info("Received delete command");
+			break;
+		case "edit":
+			conf.logger.info("Received edit command");
+			break;
+		}
+		if(conf.procNum==3)
+			nc.sendMsg(1, "NO");
+		else
+			nc.sendMsg(1, "YES");
+	}
+	/**/	
+	/*
+	public Participant(int id,Config conf){		
 		this.id = id;
 		this.viewNumber = 0;
 		this.conf = conf;
-		this.playList = new PlayList();
+		conf.procNum = id;
+		this.playList= new PlayList();
+		nc = new NetController(conf);
 	}
-	
-	void sendMessage(){
-		
+	 */
+	void sendMessage(int process, String msg){
+		nc.sendMsg(process, msg);
+		conf.logger.info("Process "+id+" sent "+ msg +" to Process "+process);
+	}
+
+	List<List<String>> getReceivedMsgs(){
+		List<List<String>> recvMsgs = nc.getReceivedMsgs();
+		for(List<String> s:recvMsgs){
+			conf.logger.info("Process "+id+" received " + s);
+		}
+		return recvMsgs;	
+	}
+
+	public void shutdown() {
+		nc.shutdown();		
+	}
+
+	public static boolean invokeThreePC(String message, String s1, String s2){
+		// Vote REQ
+		log("Sending VOTE_REQ");
+		broadcast("VOTE_REQ##"+message+"##"+s1+"##"+s2);
+
+		// Collect votes
+		List<String> exp = new ArrayList<String>();
+		exp.add("YES");
+		exp.add("NO");
+		List<String> votes=collectResults(exp);	
+
+		// Decide "PC" or Abort
+		boolean isAbort = false;
+		for(String vote : votes){
+			if(vote.equals("NO")){
+				isAbort = true;
+				log("Deciding ABORT");
+				broadcast("ABORT");
+				return false;
+			}			
+		}
+		if(!isAbort){
+			broadcast("PRECOMMIT");
+			log("Decided PRECOMMIT");
+		}
+
+		//may need to send abort else send rpecommti and wait for ack
+		exp = new ArrayList<String>();
+		exp.add("ACK");
+		List<String> acks=collectResults(exp);
+		broadcast("COMMIT");
+		log("Decided COMMIT");
+		// Send "Commit" 
+
+		return true;
+	}
+
+	static void broadcast(String msg){
+		for(int i = 1; i < conf.numProcesses;i++)
+			if(i!=conf.procNum)
+				nc.sendMsg(i, msg);
+	}
+
+	static List<String> collectResults(List<String> expectedAnswers)
+	{
+		Boolean[] check = new Boolean[conf.numProcesses-1];
+		check[conf.procNum] = true;
+		List<String> votes = new ArrayList<String>();
+		while(true){
+			List<List<String>> currVotes= nc.getReceivedMsgs();
+			for(List<String> s:currVotes){
+				check[Integer.parseInt(s.get(0))] = true;
+				if(!expectedAnswers.contains(s.get(1))){
+					conf.logger.severe("Invalid response received "+s);
+				}	
+				else
+					votes.add(s.get(1));
+			}
+			Boolean checkall = true;
+			for(Boolean t: check)
+				checkall = checkall && t;
+			if(votes.size()==conf.numProcesses-2 && checkall)
+				break;
+		}
+		return votes;
+	}
+
+	static void log(String msg){
+		conf.logger.info(msg);
 	}
 }
